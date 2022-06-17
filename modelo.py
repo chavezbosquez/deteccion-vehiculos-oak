37de9754eb562d47869de562d0fec023a282996d
chavezbosquez <chavez.bosquez@gmail.com>
2022-06-17 11:19:13 -0500

    Detección de motocicletas

--- modelo.py
+++ modelo.py
@@ -16,77 +16,68 @@ cam_rgb.setInterleaved(False)
 
 # Red neuronal para hacer las detecciones
 detection_nn = pipeline.createMobileNetDetectionNetwork()
-# Blob is the Neural Network file, compiled for MyriadX. It contains both the definition and weights of the model
-# We're using a blobconverter tool to retreive the MobileNetSSD blob automatically from OpenVINO Model Zoo
 detection_nn.setBlobPath(blobconverter.from_zoo(name='mobilenet-ssd', shaves=6))
-# Next, we filter out the detections that are below a confidence threshold. Confidence can be anywhere between <0..1>
-detection_nn.setConfidenceThreshold(0.1)
-# Next, we link the camera 'preview' output to the neural network detection input, so that it can produce detections
+detection_nn.setConfidenceThreshold(0.01)
 cam_rgb.preview.link(detection_nn.input)
 
-# XLinkOut is a "way out" from the device. Any data you want to transfer to host need to be send via XLink
 xout_rgb = pipeline.createXLinkOut()
-# For the rgb camera output, we want the XLink stream to be named "rgb"
 xout_rgb.setStreamName("rgb")
-# Linking camera preview to XLink input, so that the frames will be sent to host
 cam_rgb.preview.link(xout_rgb.input)
-
-# The same XLinkOut mechanism will be used to receive nn results
 xout_nn = pipeline.createXLinkOut()
 xout_nn.setStreamName("nn")
 detection_nn.out.link(xout_nn.input)
 
 azul = (255, 0, 0)
 verde = (36, 255, 12)
+rojo = (0, 0, 255)
+blanco = (255, 255, 255)
+negro  = (0, 0, 0)
 titulo = "Prueba de laboratorio"
 
 def displayFrame(frame):
     for detection in detections:
+        # Autos
         if detection.label == 7:
             bbox = frameNorm(frame, (detection.xmin, detection.ymin, detection.xmax, detection.ymax))
             #cv2.putText(frame, "Auto", (bbox[0]+10, bbox[1]+20), cv2.FONT_HERSHEY_TRIPLEX, 0.5, color)
             #cv2.putText(frame, f"{int(detection.confidence * 100)}%", (bbox[0] + 10, bbox[1] + 40), cv2.FONT_HERSHEY_TRIPLEX, 0.5, color)
             cv2.rectangle(frame, (bbox[0], bbox[1]), (bbox[2], bbox[3]), azul, 2)                            # Ventana 300x300:  #0.6        #2
-            cv2.putText(frame, 'Auto ' + f"{int(detection.confidence * 100)}%", (bbox[0], bbox[1] - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.5, verde, 1)
+            cv2.putText(frame, 'Auto ' + f"{int(detection.confidence * 100)}%", (bbox[0], bbox[1]-10), cv2.FONT_HERSHEY_SIMPLEX, 0.5, verde, 1)
+        # Bicicleta
+        if detection.label == 2:
+            cv2.rectangle(frame, (bbox[0], bbox[1]), (bbox[2], bbox[3]), rojo, 2)
+            cv2.putText(frame, 'Bici ' + f"{int(detection.confidence * 100)}%", (bbox[0], bbox[1] - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.5, azul, 1)
+        # Autobus
+        if detection.label == 6:
+            cv2.rectangle(frame, (bbox[0], bbox[1]), (bbox[2], bbox[3]), blanco, 2)
+            cv2.putText(frame, 'Autobus ' + f"{int(detection.confidence * 100)}%", (bbox[0], bbox[1] - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.5, negro, 1)
+        # Motocicleta
+        if detection.label == 14:
+            cv2.rectangle(frame, (bbox[0], bbox[1]), (bbox[2], bbox[3]), negro, 2)
+            cv2.putText(frame, 'Motocicleta ' + f"{int(detection.confidence * 100)}%", (bbox[0], bbox[1] - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.5, blanco, 1)
     # Show the frame
     cv2.imshow(titulo, frame)
 
 cv2.namedWindow(titulo, cv2.WINDOW_NORMAL)
 cv2.resizeWindow(titulo, 600,600)
 
-# Pipeline is now finished, and we need to find an available device to run our pipeline
-# we are using context manager here that will dispose the device after we stop using it
 with depthai.Device(pipeline) as device:
-    # From this point, the Device will be in "running" mode and will start sending data via XLink
-
-    # To consume the device results, we get two output queues from the device, with stream names we assigned earlier
     q_rgb = device.getOutputQueue("rgb")
-    q_nn = device.getOutputQueue("nn")
-
-    # Here, some of the default values are defined. Frame will be an image from "rgb" stream, detections will contain nn results
+    q_nn  = device.getOutputQueue("nn")
     frame = None
     detections = []
 
-    # Since the detections returned by nn have values from <0..1> range, they need to be multiplied by frame width/height to
-    # receive the actual position of the bounding box on the image
     def frameNorm(frame, bbox):
         normVals = np.full(len(bbox), frame.shape[0])
         normVals[::2] = frame.shape[1]
         return (np.clip(np.array(bbox), 0, 1) * normVals).astype(int)
-
-
-    # Main host-side application loop
     while True:
-        # we try to fetch the data from nn/rgb queues. tryGet will return either the data packet or None if there isn't any
         in_rgb = q_rgb.tryGet()
-        in_nn = q_nn.tryGet()
-
+        in_nn  = q_nn.tryGet()
         if in_rgb is not None:
-            # If the packet from RGB camera is present, we're retrieving the frame in OpenCV format using getCvFrame
             frame = in_rgb.getCvFrame()
 
         if in_nn is not None:
-            # when data from nn is received, we take the detections array that contains mobilenet-ssd results
             detections = in_nn.detections
 
         if frame is not None:
@@ -105,6 +96,5 @@ with depthai.Device(pipeline) as device:
             cv2.imshow("Prueba de laboratorio", frame)
             '''
             displayFrame(frame)
-        # at any time, you can press "q" and exit the main loop, therefore exiting the program itself
         if cv2.waitKey(1) == ord('q'):
             break
